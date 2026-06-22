5bbcef528eb9fee25b44bb41d099384622fa52dd
erisson <erisson.o@aluno.senai.br>
2026-06-22 15:55:42 -0300

    adicionado formulario com js

--- js/scripts_pessoas.js
+++ js/scripts_pessoas.js
@@ -1,26 +1,49 @@
 //PEGANDO ELEMENTOS DOM
-const formpessoa = document.querySelector('#form-pessoa')
-cosnt divLista = document.querySelector('#div-lit')
+const formPessoa = document.querySelector('#form-pessoa')
+const divLista = document.querySelector('#div-lista-pessoas')
 
 //CRIANDO ARRAY pessoas
-const pessoas =[]
+const pessoas = []
 
 
 //CAPTURAR O EVENTO submit DO FORMULÁRIO
-formpessoa.addEventListener('submit', (evt) => {
+formPessoa.addEventListener('submit', (evt) => {
     //INTERROMPER O EFEITO PADRÃO DE SUBMETER  OS DADOS DO FORMULÁRIO
     evt.preventDefault()
-    
+
     //CRIAR UM OBJETO FORMULÁRIO
+    const dadosFormPessoa = new FormData(formPessoa)
+
+    //CRIANDO OBJ LITERAL
     const pessoa = {
-        nome: dadosPessoas.get('nome'),
-        idade: dadosPessoas.get('idade'),
-        renda: dadosPessoas.get('renda'),
+        nome: dadosFormPessoa.get('nome'),
+        idade: dadosFormPessoa.get('idade'),
+        renda: dadosFormPessoa.get('renda')
     }
+    //CHAMANDO A FUNÇÃO addPessoa E PASSANDO O OBJ LITERAL pessoa
+    addPessoa(pessoa)
 })
 
-//CRIANDO A FUNÇÃO ADICIONAR PESSOA
+//LIMPAR O FORMULÁRIO
+formPessoa.reset
+
+//CRIANDO A FUNÇÃO ADICIONAR PESSOAS
 const addPessoa = (objPessoa) => {
+    //ADICIONAR O OBJETO LITERAL NO ARRAY PESSOAS
     pessoas.push(objPessoa)
-    
+
+    //APÓS ADICIONAR O OBJETO NO ARRAY CHAMA A FUNÇÃO listPessoas
+    listPessoas()
+
+}
+
+//FUNÇÃO PARA LISTAR PESSOAS DO ARRAY
+const listPessoas = () => {
+
+    //LIMPANDO DIV LISTA
+    divLista.innerHTML = ''
+    //PECORRER ARRAY pessoas COM O foreach
+    pessoas.forEach((elem, i) => {
+        divLista .innerHTML += `${i} - ${elem.nome} - ${elem.idade}, ${elem.renda} <br>`
+    })
 }
